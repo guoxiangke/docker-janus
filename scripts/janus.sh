da6e6c063f02a7fae635a3ffdcda13dc6222b0e9
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-08-06 19:23:45 +0200

    compile from git w/ event-handlers

--- scripts/janus.sh
+++ scripts/janus.sh
@@ -12,41 +12,20 @@ openssl req \
   -keyout $DEPS_HOME/certs/janus.key \
   -out $DEPS_HOME/certs/janus.pem
 
-wget https://github.com/meetecho/janus-gateway/archive/$JANUS_RELEASE.tar.gz -O  $DEPS_HOME/dl/janus.tar.gz
+# wget https://github.com/meetecho/janus-gateway/archive/$JANUS_RELEASE.tar.gz -O  $DEPS_HOME/dl/janus.tar.gz
+# cd $DEPS_HOME/dl
+# tar xf janus.tar.gz
 
 cd $DEPS_HOME/dl
-tar xf janus.tar.gz
-cd janus*
+git clone https://github.com/meetecho/janus-gateway
+cd janus-gateway
+git checkout event-handlers
+
 ./autogen.sh
 
 # TODO: fix websocket support as it should work
 ./configure --prefix=$DEPS_HOME --disable-websockets --disable-rabbitmq --disable-docs
 make
 make install
+make configs
 
-# make the janus configuration
-cat << EOF > $DEPS_HOME/etc/janus/janus.cfg
-[general]
-configs_folder=$DEPS_HOME/etc/janus
-plugins_folder=$DEPS_HOME/lib/janus/plugins
-debug_level=4
-
-[webserver]
-base_path=/janus
-threads=unlimited
-http=no
-https=yes
-secure_port=8089
-
-[admin]
-admin_base_path=/admin
-admin_threads=unlimited
-admin_http=no
-admin_https=yes
-admin_secure_port=7889
-; admin_acl=127.0.0.1
-
-[certificates]
-cert_pem=$DEPS_HOME/certs/janus.pem
-cert_key=$DEPS_HOME/certs/janus.key
-EOF

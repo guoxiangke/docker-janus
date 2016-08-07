32a40729298d8610515b6c350376e2a467f2ee7c
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-08-07 14:52:34 +0200

    restore standard secure_port

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ CFG_JANUS='/root/janus/etc/janus/janus.cfg'
 sed 's/; broadcast = yes/broadcast = yes/1' -i $CFG_JANUS
 CFG_HTTPS='/root/janus/etc/janus/janus.transport.http.cfg'
 sed 's/https = no/https = yes/1' -i $CFG_HTTPS
-sed 's/;secure_port = 8889/secure_port = 8089/1' -i $CFG_HTTPS
+sed 's/;secure_port = 8889/secure_port = 8889/1' -i $CFG_HTTPS
 
 # Generate Certs
 openssl req -x509 -newkey rsa:4086 \
